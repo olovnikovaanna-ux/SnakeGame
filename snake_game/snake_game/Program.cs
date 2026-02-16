4e6144110d2d79a359b9016ac0119c551c965367
olovnikovaanna-ux <olovnikovaanna@gmail.com>
2026-02-16 17:52:18 +0700

    функция ожидания нажатия

---
+++ snake_game/snake_game/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace snake_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("скоро здесь будет змейка ");
+            Console.WriteLine("\nнажмите любую клавишу");
+            Console.ReadKey();
+        }
+
+
+    }
+}
