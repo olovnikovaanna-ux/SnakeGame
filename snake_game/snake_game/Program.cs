40e56be9aadb8e1b6f9f46f5bf959d01b4e32903
olovnikovaanna-ux <olovnikovaanna@gmail.com>
2026-02-16 19:07:59 +0700

    кнопка выхода,ожидание нажатия кнопки esc

--- snake_game/snake_game/Program.cs
+++ snake_game/snake_game/Program.cs
@@ -1,20 +1,22 @@
-﻿namespace snake_game
-{
-    internal class Program
-    {
+﻿
 
-        static void Main(string[] args)
+int playerX = 5;
+int playerY = 10;
+bool flag = true;
+
+while (flag)
+{
+    if (Console.KeyAvailable)  //если что то нажато
+    {
+        ConsoleKey key = Console.ReadKey(true).Key;
+        if (key == ConsoleKey.Escape) //если клавиша esc нажата
         {
-            int playerX = 5;
-            int playerY = 10;
-            bool flag = true;
-            while (flag)
-            {
-                Console.Clear();
-                Console.SetCursorPosition(playerX++, playerY++);
-                Console.WriteLine("@");
-                Thread.Sleep(100);
-            }
+            flag = false;
+            continue;
         }
     }
+    Console.Clear();
+    Console.SetCursorPosition(playerX++, playerY++);
+    Console.WriteLine("@");
+    Thread.Sleep(100);
 }
