4f3bebc054a75eb70fe3d37ec7dac0dd521fce12
olovnikovaanna-ux <olovnikovaanna@gmail.com>
2026-02-18 20:49:36 +0700

    добавленно звуковое сопровождение и кнопка для запуска игры

--- snake_game/snake_game/Program.cs
+++ snake_game/snake_game/Program.cs
@@ -4,26 +4,38 @@ int playerX = 5;
 int playerY = 10;
 bool flag = true;
 
-while (flag)
+Console.WriteLine("НАЖМИТЕ ENTER ЧТОБЫ НАЧАТЬ ИГРУ");
+ConsoleKey key2 = Console.ReadKey(true).Key;
+if (key2 == ConsoleKey.Enter) 
 {
-    if (Console.KeyAvailable)  //если что то нажато
+    Console.Beep(400, 300);
+    while (flag)
     {
-        ConsoleKey key = Console.ReadKey(true).Key;
-        if (key == ConsoleKey.Escape) //если клавиша esc нажата
+        if (Console.KeyAvailable)  //если что то нажато
         {
-            flag = false;
-            Console.Beep(500, 200); // 500-частота гц, 200-длительность
-            continue;
-        }
+            ConsoleKey key = Console.ReadKey(true).Key;
+            if (key == ConsoleKey.Escape) //если клавиша esc нажата
+            {
+                flag = false;
+                Console.Beep(500, 200); // 500-частота гц, 200-длительность
+                continue;
+            }
 
-        ConsoleKey key1 = Console.ReadKey(true).Key;
-        if (key == ConsoleKey.P)
-        {
-            Console.SetCursorPosition(playerX--, playerY--);
+            ConsoleKey key1 = Console.ReadKey(true).Key;
+            if (key == ConsoleKey.P)
+                
+            {                
+                Console.SetCursorPosition(playerX--, playerY--);
+                
+            }
+            
         }
+        Console.Clear();
+        Console.SetCursorPosition(playerX++, playerY++);
+        Console.Beep(2000,150);
+        Console.WriteLine("@");
+        Thread.Sleep(200);
+
+        
     }
-    Console.Clear();
-    Console.SetCursorPosition(playerX++, playerY++);
-    Console.WriteLine("@");
-    Thread.Sleep(100);
 }
