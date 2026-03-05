7bd59bbf38560e8ce26102706f817e1da70df1c0
olovnikovaanna-ux <olovnikovaanna@gmail.com>
2026-03-05 17:13:01 +0700

    рамка
    ! не рисуется правая сторона рамки, рамка исчезает после начала игры

--- snake_game/snake_game/Program.cs
+++ snake_game/snake_game/Program.cs
@@ -17,13 +17,61 @@ internal class Program
         int _playerX = 5;
         int _playerY = 10;
         bool flag = true;
-       
+
+        //char BORDER_CHAR = '#';
+        //int OFFSET = 5;
+        //int fps = 100;
+        //int fieldWidth;
+        //int fieldHeight;
+
+        //Console.CursorVisible = false;
+        ////Console.WindowWidth = 60;                   
+        ////Console.WindowHeight = 30;                  
+        //fieldWidth = Console.WindowWidth;
+        //fieldHeight = Console.WindowHeight;
+
+        //int width = 77;
+        //int height = 100;
+        //int offset = 3;
+        //char borderChar = '#';
+
+
+        //int upperBorder = offset;           // верхняя граница поля
+        //int lowerBorder = offset + height;  // нижняя граница поля
+        //int leftBorder = offset;            // левая граница поля
+        //int rightBorder = offset + width;   // правая граница поля
 
 
         SetCursorPosition(playerX: 43, playerY: 3);
         WriteColor("ЧТОБЫ НАЧАТЬ ИГРУ, НАЖМИТЕ", ConsoleColor.Green);  
-        WriteColor(" ENTER ");
+        WriteColor(" ENTER\n");
 
+
+        for (int i = 0; i < 101; i++)
+        {
+            Console.Write("#");
+
+        }
+        SetCursorPosition(0, 25);
+        for (int i = 0; i < 101; i++)
+        {
+            Console.Write("#");
+
+        }
+
+        SetCursorPosition(0, 4);
+        for (int i = 0; i < 21; i++)
+        {
+            Console.WriteLine("#");
+
+        }
+
+        SetCursorPosition(101, 4);
+        for (int i = 0; i < 21; i++)
+        {
+            Console.WriteLine("#");
+
+        }
 
         ConsoleKey key2 = Console.ReadKey(true).Key; // TO DO:
 
@@ -36,56 +84,44 @@ internal class Program
             while (flag)
             {
 
-                char BORDER_CHAR = '#';
-                int OFFSET = 5;
-                int fps = 100;
-                int fieldWidth;
-                int fieldHeight;
 
-                Console.CursorVisible = false;                
-                //Console.WindowWidth = 60;                   
-                //Console.WindowHeight = 30;                  
-                fieldWidth = Console.WindowWidth;          
-                fieldHeight = Console.WindowHeight;
+                //Console.Clear();
 
-                int width = 77;
-                int height = 100;
-                int offset = 3;
-                char borderChar = '#';
-    
+                //for (int x = leftBorder; x < rightBorder; x++)
+                //{
+                //    Console.SetCursorPosition(x, upperBorder);
+                //    Console.Write(borderChar);
+                //}
 
-        int upperBorder = offset;           // верхняя граница поля
-        int lowerBorder = offset + height;  // нижняя граница поля
-        int leftBorder = offset;            // левая граница поля
-        int rightBorder = offset + width;   // правая граница поля
+                //for (int x = leftBorder; x < rightBorder; x++)
+                //{
+                //    Console.SetCursorPosition(x, lowerBorder);
+                //    Console.Write(borderChar);
+                //}
 
-        Console.Clear();
+                //for (int y = upperBorder; y < lowerBorder; y++)
+                //{
+                //    Console.SetCursorPosition(leftBorder, y);
+                //    Console.Write(borderChar);
+                //}
 
-        for (int x = leftBorder; x < rightBorder; x++)
-        {
-            Console.SetCursorPosition(x, upperBorder);
-            Console.Write(borderChar);
-        }
+                //for (int y = upperBorder; y < lowerBorder; y++)
+                //{
+                //    Console.SetCursorPosition(rightBorder, y);
+                //    Console.Write(borderChar);
+                //}
 
-        for (int x = leftBorder; x < rightBorder; x++)
-        {
-            Console.SetCursorPosition(x, lowerBorder);
-            Console.Write(borderChar);
-        }
 
-        for (int y = upperBorder; y < lowerBorder; y++)
-        {
-            Console.SetCursorPosition(leftBorder, y);
-            Console.Write(borderChar);
-        }
 
-        for (int y = upperBorder; y < lowerBorder; y++)
-        {
-            Console.SetCursorPosition(rightBorder, y);
-            Console.Write(borderChar);
-        }
+                if (_playerX == 100)
+                {
+                    _playerX = 0;
+                }
 
-    
+                if (_playerY == 24)
+                {
+                    _playerY = 4;
+                }
 
                 if (Console.KeyAvailable)  //если что то нажато
                 {
@@ -151,6 +187,7 @@ internal class Program
                 //    Console.WriteLine("@");
                 //    Thread.Sleep(50);
                 //}
+
 
                 Console.Clear();
                 SetCursorPosition(_playerX, _playerY);
